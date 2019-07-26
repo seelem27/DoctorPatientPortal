c1440f61fe1de6f347b59974dff6f51203f338f4
seelem <dzyenlem@gmail.com>
2019-07-27 04:04:12 +0800

    fix on appointment monthView time display issue

--- resources/views/admin/appointments/index.blade.php
+++ resources/views/admin/appointments/index.blade.php
@@ -28,6 +28,12 @@
     <!-- AdminLTE Skins. Choose a skin from the css/skins
         folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+
+    <style>
+        .fc-month-view span.fc-title{
+              white-space: normal;
+        }
+    </style>
     
     <section class="box">
         <div class="row">
@@ -127,140 +133,148 @@
     <script src="../../plugins/datatables/dataTables.bootstrap4.js"></script>
 
     <script>
-            $(function () {
-              /* initialize the external events
-               -----------------------------------------------------------------*/
-              function init_events(ele) {
-                ele.each(function () {
-                  // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
-                  // it doesn't need to have a start or end
-                  var eventObject = {
-                    title: $.trim($(this).text()) // use the element's text as the event title
-                  }
-                  // store the Event Object in the DOM element so we can get to it later
-                  $(this).data('eventObject', eventObject)
-                  // make the event draggable using jQuery UI
-                  $(this).draggable({
-                    zIndex        : 1070,
-                    revert        : true, // will cause the event to go back to its
-                    revertDuration: 0  //  original position after the drag
-                  })
-                })
+        $(function () {
+          /* initialize the external events
+           -----------------------------------------------------------------*/
+          function init_events(ele) {
+            ele.each(function () {
+              // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
+              // it doesn't need to have a start or end
+              var eventObject = {
+                title: $.trim($(this).text()) // use the element's text as the event title
               }
-              init_events($('#external-events div.external-event'))
-              /* initialize the calendar
-               -----------------------------------------------------------------*/
-              //Date for the calendar events (dummy data)
-              var date = new Date()
-              var d    = date.getDate(),
-                  m    = date.getMonth(),
-                  y    = date.getFullYear()
-              $('#calendar').fullCalendar({
-                header    : {
-                  left  : 'prev,next today',
-                  center: 'title',
-                  right : 'month,agendaWeek,agendaDay'
-                },
-                minTime: "08:00:00",
-                maxTime: "22:00:00",
-                buttonText: {
-                  today: 'today',
-                  month: 'month',
-                  week : 'week',
-                  day  : 'day'
-                },
-                plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
-                selectable: true,
-                selectOverlap: false,
-                select: function(start, end) {                    
-                    var cnf = confirm('Create appointment at ' + start.format("YYYY-MM-DD HH:mm:ss") + ' to ' + end.format("YYYY-MM-DD HH:mm:ss"));
-                    if(cnf == true) {
-                        date = date;
-                        start = start_time;
-                        end = finish_time;
-                        $.ajax({
-                            url: 'admin.appointments.create',
-                            type: 'POST',
-                            data: {
-                                date: date,
-                                start: start_time,
-                                end: finish_time,
-                            },
-                        })
-                    }
-                    else
-                    {
-
-                    }
-                },
-                    
-                //Random default events
-                events    : [
-                    @foreach($appointments as $appointment)
-                    {
-                        title : '{{ $appointment->patient->name }}',
-                        start : '{{ $appointment->start_time }}',
-                        @if ($appointment->finish_time)
-                                end: '{{ $appointment->finish_time }}',
-                        @endif
-                        url : '{{ route('admin.appointments.edit', $appointment->id) }}'
-                    },
-                    @endforeach
-                ],
-                editable  : false,
-                droppable : false, // this allows things to be dropped onto the calendar !!!
-                drop      : function (date, allDay) { // this function is called when something is dropped
-                  // retrieve the dropped element's stored Event Object
-                  var originalEventObject = $(this).data('eventObject')
-                  // we need to copy it, so that multiple events don't have a reference to the same object
-                  var copiedEventObject = $.extend({}, originalEventObject)
-                  // assign it the date that was reported
-                  copiedEventObject.start           = date
-                  copiedEventObject.allDay          = allDay
-                  copiedEventObject.backgroundColor = $(this).css('background-color')
-                  copiedEventObject.borderColor     = $(this).css('border-color')
-                  // render the event on the calendar
-                  // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
-                  $('#calendar').fullCalendar('renderEvent', copiedEventObject, true)
-                  // is the "remove after drop" checkbox checked?
-                  if ($('#drop-remove').is(':checked')) {
-                    // if so, remove the element from the "Draggable Events" list
-                    $(this).remove()
-                  }
-                }
-              })
-              /* ADDING EVENTS */
-              var currColor = '#3c8dbc' //Red by default
-              //Color chooser button
-              var colorChooser = $('#color-chooser-btn')
-              $('#color-chooser > li > a').click(function (e) {
-                e.preventDefault()
-                //Save color
-                currColor = $(this).css('color')
-                //Add color effect to button
-                $('#add-new-event').css({ 'background-color': currColor, 'border-color': currColor })
-              })
-              $('#add-new-event').click(function (e) {
-                e.preventDefault()
-                //Get value and make sure it is not null
-                var val = $('#new-event').val()
-                if (val.length == 0) {
-                  return
-                }
-                //Create events
-                var event = $('<div />')
-                event.css({
-                  'background-color': currColor,
-                  'border-color'    : currColor,
-                  'color'           : '#fff'
-                }).addClass('external-event')
-                event.html(val)
-                $('#external-events').prepend(event)
-                //Add draggable funtionality
-                init_events(event)
-                //Remove event from text input
-                $('#new-event').val('')
+              // store the Event Object in the DOM element so we can get to it later
+              $(this).data('eventObject', eventObject)
+              // make the event draggable using jQuery UI
+              $(this).draggable({
+                zIndex        : 1070,
+                revert        : true, // will cause the event to go back to its
+                revertDuration: 0  //  original position after the drag
               })
             })
+          }
+          init_events($('#external-events div.external-event'))
+          /* initialize the calendar
+           -----------------------------------------------------------------*/
+          //Date for the calendar events (dummy data)
+          var date = new Date()
+          var d    = date.getDate(),
+              m    = date.getMonth(),
+              y    = date.getFullYear()
+          $('#calendar').fullCalendar({
+            header    : {
+              left  : 'prev,next today',
+              center: 'title',
+              right : 'month,agendaWeek,agendaDay'
+            },
+            windowResize: true,
+            //defaultView: 'agendaDay',
+            contentHeight: 'auto',
+            minTime: "07:00:00",
+            maxTime: "22:00:00",
+            buttonText: {
+              today: 'today',
+              month: 'month',
+              week : 'week',
+              day  : 'day'
+            },
+            //plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
+            //selectable: true,
+            //selectOverlap: false,
+            // select: function(start, end) {                    
+            //     var cnf = confirm('Create appointment at ' + start.format("YYYY-MM-DD HH:mm:ss") + ' to ' + end.format("YYYY-MM-DD HH:mm:ss"));
+            //     if(cnf == true) {
+            //         date = date;
+            //         start = start_time;
+            //         end = finish_time;
+            //         $.ajax({
+            //             url: 'admin.appointments.create',
+            //             type: 'POST',
+            //             data: {
+            //                 date: date,
+            //                 start: start_time,
+            //                 end: finish_time,
+            //             },
+            //         })
+            //     }
+            //     else
+            //     {
+            //     }
+            // },
+                
+            //Random default events
+            events    : [
+                @foreach($appointments as $appointment)
+                {
+                    title : '{{ $appointment->patient->name }}',
+                    start : '{{ $appointment->start_time }}',
+                    description: 'Medical Checkup',
+                    @if ($appointment->finish_time)
+                            end: '{{ $appointment->finish_time }}',
+                    @endif
+                    url : '{{ route('admin.appointments.edit', $appointment->id) }}'
+                },
+                @endforeach
+            ],
+            eventRender: function(event, element) {
+                element.find('.fc-title').append("<br/>" + event.description);
+            },
+            timeFormat: 'h(:mm)a',
+            displayEventEnd: true,
+            editable  : false,
+            droppable : false, // this allows things to be dropped onto the calendar !!!
+            drop      : function (date, allDay) { // this function is called when something is dropped
+              // retrieve the dropped element's stored Event Object
+              var originalEventObject = $(this).data('eventObject')
+              // we need to copy it, so that multiple events don't have a reference to the same object
+              var copiedEventObject = $.extend({}, originalEventObject)
+              // assign it the date that was reported
+              copiedEventObject.start           = date
+              copiedEventObject.allDay          = allDay
+              copiedEventObject.backgroundColor = $(this).css('background-color')
+              copiedEventObject.borderColor     = $(this).css('border-color')
+              // render the event on the calendar
+              // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+              $('#calendar').fullCalendar('renderEvent', copiedEventObject, true)
+              // is the "remove after drop" checkbox checked?
+              if ($('#drop-remove').is(':checked')) {
+                // if so, remove the element from the "Draggable Events" list
+                $(this).remove()
+              }
+            }
+          })
+          /* ADDING EVENTS */
+          var currColor = '#3c8dbc' //Red by default
+          //Color chooser button
+          var colorChooser = $('#color-chooser-btn')
+          $('#color-chooser > li > a').click(function (e) {
+            e.preventDefault()
+            //Save color
+            currColor = $(this).css('color')
+            //Add color effect to button
+            $('#add-new-event').css({ 'background-color': currColor, 'border-color': currColor })
+          })
+          $('#add-new-event').click(function (e) {
+            e.preventDefault()
+            //Get value and make sure it is not null
+            var val = $('#new-event').val()
+            if (val.length == 0) {
+              return
+            }
+            //Create events
+            var event = $('<div />')
+            event.css({
+              'background-color': currColor,
+              'border-color'    : currColor,
+              'color'           : '#fff'
+            }).addClass('external-event')
+            event.html(val)
+            $('#external-events').prepend(event)
+            //Add draggable funtionality
+            init_events(event)
+            //Remove event from text input
+            $('#new-event').val('')
+          })
+        })
     </script>
 @endsection
